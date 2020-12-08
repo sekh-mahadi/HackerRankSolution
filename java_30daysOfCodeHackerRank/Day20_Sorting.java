29d3472c9f87a06df7e3e1adc776b90bbb10578e
msekh <msekh36@gmail.com>
2020-12-08 11:05:33 +0600

    Day 20 Sorting

---
+++ java_30daysOfCodeHackerRank/Day20_Sorting.java
@@ -0,0 +1,31 @@
+package java_30daysOfCodeHackerRank;
+
+import java.util.Scanner;
+
+public class Day20_Sorting {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for (int a_i = 0; a_i < n; a_i++) {
+            a[a_i] = in.nextInt();
+        }
+        // Write Your Code Here
+
+        int numOfSwaps = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                    numOfSwaps++;
+                }
+            }
+        }
+        System.out.println("Array is sorted in " + numOfSwaps + " swaps.");
+        System.out.println("First Element: " + a[0]);
+        System.out.println("Last Element: " + a[a.length - 1]);
+    }
+}
